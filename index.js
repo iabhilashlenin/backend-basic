62133df1cf72daded0c880e509eae150fbd3f7ce
iabhilashlenin <iabhilashlenin@gmail.com>
2024-08-14 22:04:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res) =>{
+    res.send('abhilash-dev')
+})
+
+app.get('/login',(req,res) =>{
+    res.send('<h1> please login at abhilash-dev</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>welcome to abhilash youtube</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
